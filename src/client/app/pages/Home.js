4f650d18071d71cd2c30a23f7ba2a352b9d14973
Liam Lightfoot <u6382079@anu.edu.au>
2020-09-03 16:46:04 +1000

    fixed syntax error

--- src/client/app/pages/Home.js
+++ src/client/app/pages/Home.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 
-import { GetCutePicture, GetProgrammingQuote } from '../Components/Function';
+import { GetCutePicture, GetProgrammingQuote } from '../components/Function';
 
 import './Home.css';
 
